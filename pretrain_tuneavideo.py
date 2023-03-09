60f4ead1d8a20a06f955575a8881ba668ac2ff4a
Kiet Nguyen <kanguyen.vn@gmail.com>
2023-03-08 23:40:17 -0500

    Debug

--- pretrain_tuneavideo.py
+++ pretrain_tuneavideo.py
@@ -298,8 +298,9 @@ def main(
                     progress_bar.update(1)
                 continue
 
-            videopath = Path(batch["videopath"])
-            logger.info(f"File: {str(videopath.relative_to(videopath.parents[1]))}")
+            videopaths = [Path(videopath) for videopath in batch["videopath"]]
+            for videopath in videopaths:
+                logger.info(f"File: {str(videopath.relative_to(videopath.parents[1]))}")
 
             with accelerator.accumulate(unet):
                 # Convert videos to latent space
