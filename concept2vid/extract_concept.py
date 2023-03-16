329ec907d2d1d1a73305d7c1bdcf0d92d5a1f600
Kiet Nguyen <kanguyen.vn@gmail.com>
2023-03-15 22:23:44 -0400

    Debug

--- concept2vid/extract_concept.py
+++ concept2vid/extract_concept.py
@@ -255,7 +255,7 @@ def get_models_training(weight_path):
 def get_quantized_feature(models, text, video=None, device="cpu", dtype=torch.float32):
     video_features = None
     if video is not None:
-        inputs = process_input(models["tokenizer"], video, text).to(device)
+        inputs = process_input(models["tokenizer"], video, text, dtype).to(device)
         # print(inputs.keys())
         outputs = models["model"](**inputs, output_hidden_states=True, return_dict=True)
         video_features = outputs["video_embeds"].unsqueeze(-2)
@@ -306,7 +306,9 @@ def process_input(processor, videos, texts, dtype=torch.float32):
     )
     # print(inputs["pixel_values"].shape)  # torch.Size([1, 8192, 3, 224, 224])
     _, b_t, c, h, w = inputs["pixel_values"].shape
-    inputs["pixel_values"] = Variable(inputs["pixel_values"].reshape(batch, t, c, h, w)).to(dtype=dtype)
+    inputs["pixel_values"] = Variable(
+        inputs["pixel_values"].reshape(batch, t, c, h, w)
+    ).to(dtype=dtype)
     inputs["input_ids"] = torch.Tensor(inputs["input_ids"]).to(dtype=dtype)
     # print(inputs['pixel_values'].shape) # torch.Size([256, 32, 3, 224, 224])
     # print(inputs['input_ids'].shape) # torch.Size([256, 10])
