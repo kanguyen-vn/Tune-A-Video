8b62c34879798f8e8874e9277ca5a9499693f810
Kiet Nguyen <kanguyen.vn@gmail.com>
2023-03-16 17:21:52 -0400

    Debug quantized

--- pretrain_tuneavideo.py
+++ pretrain_tuneavideo.py
@@ -286,7 +286,7 @@ def main(
         train_models["quantized_transformer_model"].to(
             accelerator.device, dtype=weight_dtype
         )
-    vae.to(accelerator.device, dtype=weight_dtype)
+    # vae.to(accelerator.device, dtype=weight_dtype)
 
     # We need to recalculate our total training steps as the size of the training dataloader may have changed.
     num_update_steps_per_epoch = math.ceil(
